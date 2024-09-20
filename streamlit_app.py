324a5f137bd0b46e0a44ac120c666286f015d27a
McNamara <holgerehrmann@gmail.com>
2024-09-20 07:01:57 +0000

    added dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
-
+import matplotlib.pyplot as plt
+file_path = 'data/BastarCraton.csv'
+df = pd.read_csv(file_path)
+st.dataframe(df)
 st.title('Hello World!')
 st.write('*Data Science Course*')
-st.container(height=400, border=True).write("Test")
+st.container(height=400, border=True).write(df)

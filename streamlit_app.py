aae57417f8463b3929e1dddf8c99486b21169541
McNamara <holgerehrmann@gmail.com>
2024-09-20 07:25:47 +0000

    added matplotlib

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
 file_path = 'data/BastarCraton.csv'
 df = pd.read_csv(file_path)
 #st.dataframe(df)

e859eee0b09b37b606672a5b853af466eb47bf7c
McNamara <holgerehrmann@gmail.com>
2024-09-20 07:18:17 +0000

    added dropdown

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 file_path = 'data/BastarCraton.csv'
 df = pd.read_csv(file_path)
-st.dataframe(df)
+#st.dataframe(df)
+cat_names = df.columns.to_list()[27:]
 st.title('Hello World!')
 st.write('*Data Science Course*')
-st.container(height=400, border=True).write(df)
+el1 = st.selectbox('Select element', cat_names)
+#st.container(height=400, border=True)
